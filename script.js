836f7e96904d3e90be51aa590a3400bd348ed64e
Amit Ranan <ranan.amit@gmail.com>
2015-09-07 18:08:10 -0700

    functional

--- script.js
+++ script.js
@@ -70,43 +70,50 @@ var quiz = {
 
 var numberOfCorrectAnswers = 0;
 
-
 function displayQuizQuestion() {
-	var position = quiz.currentQuestion;
-	var item = quiz.items[position];
 
-	//appends Question Number
-	$(".question-number").html('Question #' + (position + 1));
+	if (quiz.currentQuestion < 5) {
+		var position = quiz.currentQuestion;
+		var item = quiz.items[position];
 
-
-	// appends the actual question
-	$('#question').html(item.question); 
-	var questionsContainer = $('#questions-container');
-	questionsContainer.html('');
+		//appends Question Number
+		$(".question-number").html('Question #' + (position + 1));
 
 
-	// appends the next button
-	var nextButtonContainer = $("#next-button-container");
-	nextButtonContainer.html('<input class="next-button" type="submit" value="next">');
+		// appends the actual question
+		$('#question').html(item.question); 
+		var questionsContainer = $('#questions-container');
+		questionsContainer.html('');
 
 
-	// appends multiple choice answers
-	for (var i = 0; i < item.options.length; i++) { 
-		var option = $('<input class="input-one" type="radio" id="q' + i + '" name="quiz-item" value="' + i +'" required><label for="q' + i + '">' + item.options[i] + '</label><br/>');
-		questionsContainer.append(option);
+		// appends the next button
+		var nextButtonContainer = $("#next-button-container");
+		nextButtonContainer.html('<input class="next-button" type="submit" value="next">');
+
+
+		// appends multiple choice answers
+		for (var i = 0; i < item.options.length; i++) { 
+			var option = $('<input class="input-one" type="radio" id="q' + i + '" name="quiz-item" value="' + i +'" required><label for="q' + i + '">' + item.options[i] + '</label><br/>');
+			questionsContainer.append(option);
+		}
+
+		// checks user answer and adds 1 to var numberOfCorrectAnswers if correct
+		$("input[type='submit']").click(function(){
+	            var radioValue = $("input[name='quiz-item']:checked").val();
+	            if(radioValue == item.correctAnswer) {
+	                numberOfCorrectAnswers++;
+	            }
+	    });
+
+		// appends number of correct answers on score page
+		$(".score-page span").html(numberOfCorrectAnswers);
+
 	}
 
-
-
-	$("input[type='submit']").click(function(){
-            var radioValue = $("input[name='quiz-item']:checked").val();
-            if(radioValue == item.correctAnswer) {
-                numberOfCorrectAnswers++;
-                alert(numberOfCorrectAnswers);
-            }
-        });
-
-
+	else {
+		$(".score-page").show();
+		$(".question-1").hide();
+	}
 };
 
 function moveToNext() {
@@ -123,6 +130,9 @@ $(document).ready(function() {
 		displayQuizQuestion();
 	});
 
+	$(".try-again-button").click(function() {
+		location.reload();
+	});
 
 });
 
